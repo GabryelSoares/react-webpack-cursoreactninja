c7bb9340ae90f881bc97728c832097f052e5ac13
gabryelfhsoares <gabryelfhsoares@gmail.com>
2020-10-09 19:52:09 -0300

    M1#A11 - Renderizando componentes com funções puras

--- src/title.js
+++ src/title.js
@@ -2,18 +2,27 @@
 
 import React from 'react'
 
-const Title = React.createClass({
-  getDefaultProps: function () {
-    return {
-      name: 'Anônimo',
-      lastname: 'sem sobrenome'
-    }
-  },
-  render: function () {
-    return (
-      <h1>Olá {this.props.name + ' ' + this.props.lastname}!</h1>
-    )
-  }
-})
+const Title = ({ name, lastname }) => (
+  <h1>Olá {`${name} ${lastname}!`}</h1>
+)
+
+Title.defaultProps = {
+  name: 'Anônimo',
+  lastname: 'sem sobrenome'
+}
+
+// const Title = React.createClass({
+//   getDefaultProps: function () {
+//     return {
+//       name: 'Anônimo',
+//       lastname: 'sem sobrenome'
+//     }
+//   },
+//   render: function () {
+//     return (
+//       <h1>Olá {this.props.name + ' ' + this.props.lastname}!</h1>
+//     )
+//   }
+// })
 
 export default Title
