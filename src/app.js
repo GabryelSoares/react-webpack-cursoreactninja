0e0e1ae16be887464be8b2ce66756405667a0166
gabryelfhsoares <gabryelfhsoares@gmail.com>
2020-10-14 19:29:07 -0300

    M1#A28 - Introdução à formulários no React

--- src/app.js
+++ src/app.js
@@ -2,30 +2,24 @@
 
 import React, { Component } from 'react'
 import Button from './button'
-import Square from './square'
-import Timer from './timer'
+
 class App extends Component {
   constructor() {
-    console.log('constructor')
     super()
     this.state = {
-      time: 0,
-      showTimer: true
+      value: 'Valor inicial'
     }
   }
-
-  componentWillMount () {
-    console.log('componentWillMount')
-  }
-
   render () {
-    console.log('render')
     return (
       <div>
-        <Timer time={this.state.time} />
-        <button onClick={() => {
-          this.setState({ time: this.state.time + 10 })
-        }}>Change props</button>
+        <form>
+          <input type="text" value={this.state.value} onChange={(e) => {
+            this.setState({
+              value: e.target.value
+            })
+          }} />
+        </form>
       </div>
     )
   }
