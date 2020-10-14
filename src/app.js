bc8acc6d157a5cde17338d444d5a3743a87adbb2
gabryelfhsoares <gabryelfhsoares@gmail.com>
2020-10-14 14:30:05 -0300

    M1#A20 - Stateful vs stateless

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
 'use strict'
 
 import React, { Component } from 'react'
+import Button from './button'
+import Square from './square'
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      text: 'Gabryel'
+      color: green
     }
   }
   render () {
     return (
-      <div className='container' onClick={() => (this.setState({
-        text: 'Soares'
-      }))}>
-        {this.state.text}
+      <div>
+        <Square color={this.state.color} />
+
+        {['red', 'green', 'blue'].map((color) => (
+          <Button key={color} handleClick={() => this.setState({ color })}>
+            {color}
+          </Button>
+        ))}
+
       </div>
     )
   }
