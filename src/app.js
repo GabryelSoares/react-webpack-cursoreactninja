7c4b3a168a49177cbcaf9fdc71cc1af096a88f66
gabryelfhsoares <gabryelfhsoares@gmail.com>
2020-10-15 18:19:36 -0300

    M1#A41 - GitHub app - Refatorando/populando dados

--- src/app.js
+++ src/app.js
@@ -1,35 +1,47 @@
 'use strict'
 
 import React, { Component } from 'react'
+import ajax from '@fdaciuk/ajax'
 import AppContent from './components/app-content'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      userinfo: {
-        username: 'Gabryel Soares',
-        photo: 'https://avatars3.githubusercontent.com/u/55500531?v=4',
-        login: 'GabryelSoares',
-        repos: 5,
-        followers: 0,
-        following: 1
-      },
-      repos: [{
-        name: 'Repo',
-        link: '#'
-      }],
-      starred: [{
-        name: 'Repo',
-        link: '#'
-      }]
+      userinfo: null,
+      repos: [],
+      starred: []
     }
   }
+
+  handleSearch (e) {
+    const value = e.target.value
+    const keyCode = e.which || e.keyCode
+    const ENTER = 13
+
+    if (keyCode === ENTER) {
+      ajax().get(`https://api.github.com/users/${ value }`).then((result) => {
+        console.log(result)
+        this.setState({
+          userinfo: {
+            username: result.name,
+            photo: result.avatar_url,
+            login: result.login,
+            repos: result.public_repos,
+            followers: result.followers,
+            following: result.following
+          }
+        })
+      })
+    }
+  }
+
   render () {
     return <AppContent
       userinfo={this.state.userinfo}
       repos={this.state.repos}
       starred={this.state.starred}
+      handleSearch={(e) => this.handleSearch(e)}
     />
   }
 }
