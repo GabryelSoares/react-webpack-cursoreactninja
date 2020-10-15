bf9a59a3ff84f988a2be5167646080825ce3f332
gabryelfhsoares <gabryelfhsoares@gmail.com>
2020-10-15 16:48:34 -0300

    M1#A37 - GitHub app - Criando Container Component

--- src/app.js
+++ src/app.js
@@ -1,37 +1,20 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
+import AppContent from './components/app-content'
 
-import Search from './components/search'
-import UserInfo from './components/user-info'
-import Actions from './components/actions'
-import Repos from './components/repos'
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
 
-const App = () => (
-  <div className='app'>
-    <Search />
-    <UserInfo />
-    <Actions />
+    }
+  }
+  render () {
+    return <AppContent
 
-    <Repos
-      className='repos'
-      title='Repositórios: '
-      repos={[{
-        name: 'Nome do repositório',
-        link: '#'
-      }]}
     />
-
-    <Repos
-      className='starred'
-      title='Favoritos: '
-      repos={[{
-        name: 'Nome do repositório',
-        link: '#'
-      }]}
-    />
-
-  </div>
-)
+  }
+}
 
 export default App
---
+++ src/components/app-content.js
@@ -0,0 +1,41 @@
+'use strict'
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Search from './search'
+import UserInfo from './user-info'
+import Actions from './actions'
+import Repos from './repos'
+
+const AppContent = () => (
+  <div className='app'>
+    <Search />
+    <UserInfo />
+    <Actions />
+
+    <Repos
+      className='repos'
+      title='Repositórios: '
+      repos={[{
+        name: 'Nome do repositório',
+        link: '#'
+      }]}
+    />
+
+    <Repos
+      className='starred'
+      title='Favoritos: '
+      repos={[{
+        name: 'Nome do repositório',
+        link: '#'
+      }]}
+    />
+  </div>
+)
+
+AppContent.defaultProps = {}
+
+AppContent.propTypes = {}
+
+export default AppContent
