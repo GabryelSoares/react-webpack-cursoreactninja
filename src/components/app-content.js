cb02f277faa7a1dd5cebabdf574853f7f597aec5
gabryelfhsoares <gabryelfhsoares@gmail.com>
2020-10-16 14:44:02 -0300

    M1#A46 - GitHub app - loading

--- src/components/app-content.js
+++ src/components/app-content.js
@@ -11,12 +11,14 @@ const AppContent = ({
   userinfo,
   repos,
   starred,
+  isFetching,
   handleSearch,
   getRepos,
   getStarred
 }) => (
     <div className='app'>
-      <Search handleSearch={handleSearch} />
+      <Search isDisabled={isFetching} handleSearch={handleSearch} />
+      {isFetching && <div>Carregando...</div>}
       {!!userinfo && <UserInfo userinfo={userinfo} />}
       {!!userinfo && <Actions getRepos={getRepos} getStarred={getStarred} />}
 
@@ -39,7 +41,11 @@ AppContent.defaultProps = {}
 AppContent.propTypes = {
   userinfo: PropTypes.object,
   repos: PropTypes.array.isRequired,
-  starred: PropTypes.array.isRequired
+  starred: PropTypes.array.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  handleSearch: PropTypes.func.isRequired,
+  getRepos: PropTypes.func.isRequired,
+  getStarred: PropTypes.func.isRequired
 }
 
 export default AppContent
